742b4e781f405d4e6f9f8313b9925157c8959bb0
arybkovskyi <alex.rybkovskyi@gmail.com>
2022-09-21 02:23:31 -0500

    auth dirty test

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,10 +1,33 @@
 import {expect} from "chai";
+import supertest from 'supertest'
 
-describe('example', ()=>{
-    it('MATH', ()=>{
-        const a = 2;
-        const b = 3;
-        const c = a + b;
-        expect(c).to.eq(5)
+describe('AUTH', () => {
+    const require = supertest('https://paysis.herokuapp.com');
+    //example of the same test without chai (ex use done)
+    /* it('Successfull login', (done)=>{
+         const require = supertest('https://paysis.herokuapp.com');
+         require
+             .post('/auth').send({login: 'adminius', password: 'supers3cret'})
+             .expect(200, done)
+             })*/
+    it('Successfull login', () => {
+        require
+            .post('/auth')
+            .send({login: 'adminius', password: 'supers3cret'})
+            .end(function (err, res) {
+                expect(res.statusCode).to.eq(300)
+                expect(res.body.token).to.be.undefined
+            })
     })
+    it('Invalid login', () => {
+        const require = supertest('https://paysis.herokuapp.com');
+        require
+            .post('/auth')
+            .send({login: 'Invalid', password: 'Invalid'})
+            .end(function (err, res) {
+                expect(res.statusCode).to.eq(404)
+                expect(res.body.message).to.eq('Wrong login or password')
+            })
+    })
+
 })
