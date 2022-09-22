426a4fe1099ff4dbaa0826c2dceb290a8bfb4497
arybkovskyi <alex.rybkovskyi@gmail.com>
2022-09-22 01:17:16 -0500

    environment variables

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,8 +1,9 @@
 import {expect} from "chai";
-import supertest from 'supertest'
+import supertest from 'supertest';
+import 'dotenv/config'
 
 describe('AUTH', () => {
-    const require = supertest('https://paysis.herokuapp.com');
+    const require = supertest(process.env.BASE_URL);
     //example of the same test without chai (ex use done)
     /* it('Successfull login', (done)=>{
          const require = supertest('https://paysis.herokuapp.com');
@@ -13,17 +14,17 @@ describe('AUTH', () => {
     it('Successfull login', () => {
         require
             .post('/auth')
-            .send({login: 'adminius', password: 'supers3cret'})
+            .send({login: process.env.login, password: process.env.password})
             .end(function (err, res) {
                 expect(res.statusCode).to.eq(300)
                 expect(res.body.token).to.be.undefined
             })
     })
     it('Invalid login', () => {
-        const require = supertest('https://paysis.herokuapp.com');
+        const require = supertest(process.env.BASE_URL);
         require
             .post('/auth')
-            .send({login: 'Invalid', password: 'Invalid'})
+            .send({login: process.env.login, password: process.env.password})
             .end(function (err, res) {
                 expect(res.statusCode).to.eq(404)
                 expect(res.body.message).to.eq('Wrong login or password')
